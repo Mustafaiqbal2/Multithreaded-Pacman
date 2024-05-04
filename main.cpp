273e6792fca540a4cdc7098d3af4bae02645290d
Mustafaiqbal2 <i221139@nu.edu.pk>
2024-05-04 20:24:49 +0500

    basic game

--- main.cpp
+++ main.cpp
@@ -10,16 +10,14 @@
 #include <queue>
 
 // Define game board size
-#define ROWS 10
-#define COLS 10
-#define CELL_SIZE 50 // Size of each cell in pixels
+#define ROWS 25
+#define COLS 25
+#define CELL_SIZE 32 // Size of each cell in pixels
 
 using namespace std;
 using namespace sf;
 
 // Define game entities
-enum Entity { EMPTY, PACMAN, GHOST, WALL, PELLET, POWER_PELLET };
-
 // Define game grid
 int gameMap[ROWS][COLS] = {0};
 
@@ -38,6 +36,9 @@ struct Ghost {
 // Function to draw the grid with appropriate shapes for pellets, power-ups, and walls
 void drawGrid(sf::RenderWindow& window)
 {
+    sf::CircleShape pelletShape(5);
+    sf::CircleShape powerUpShape(5);
+    sf::RectangleShape wallShape(sf::Vector2f(CELL_SIZE -1, CELL_SIZE -1));
     for (int i = 0; i < ROWS; i++)
     {
         for (int j = 0; j < COLS; j++)
@@ -45,23 +46,22 @@ void drawGrid(sf::RenderWindow& window)
             // Draw pellets, power-ups, and walls
             switch (gameMap[i][j])
             {
-                case 1:
+                case 2:
                     // Draw small white circle for pellet
-                    sf::CircleShape pelletShape(5);
+                    
                     pelletShape.setFillColor(sf::Color::White);
                     pelletShape.setPosition(j * CELL_SIZE + CELL_SIZE / 2 - 5, i * CELL_SIZE + CELL_SIZE / 2 - 5);
                     window.draw(pelletShape);
                     break;
-                case 2:
+                case 3:
                     // Draw slightly bigger red circle for power-up
-                    sf::CircleShape powerUpShape(8);
+                    
                     powerUpShape.setFillColor(sf::Color::Red);
                     powerUpShape.setPosition(j * CELL_SIZE + CELL_SIZE / 2 - 8, i * CELL_SIZE + CELL_SIZE / 2 - 8);
                     window.draw(powerUpShape);
                     break;
-                case 0:
+                case 1:
                     // Draw blue rectangle for wall
-                    sf::RectangleShape wallShape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
                     wallShape.setFillColor(sf::Color::Blue);
                     wallShape.setPosition(j * CELL_SIZE, i * CELL_SIZE);
                     window.draw(wallShape);
@@ -78,18 +78,27 @@ void initializeGameBoard()
     {
         for (int j = 0; j < COLS; j++)
         {
-            if ((i == 1 && j == 1) || (i == ROWS - 2 && j == COLS - 2)) {
+            if ((i == 0 || j == 0) || (i == ROWS - 1 || j == COLS - 1)) {
                 // Fixed walls
-                gameMap[i][j] = WALL;
-            } else {
+                gameMap[i][j] = 1;
+            } 
+            /*
+            else if((i >= 0 || j == 0) || (i == ROWS - 1 || j == COLS - 1)) {
+                // Fixed walls
+                gameMap[i][j] = 1;
+            }
+            */
+            else 
+            {
                 // Randomly place pellets and power-ups
-                int randNum = rand() % 10; // Generate random number from 0 to 9
-                if (randNum == 0) {
-                    gameMap[i][j] = POWER_PELLET + rand() % 3; // Randomly assign power-up value (2, 3, or 4)
-                } else if (randNum < 4) {
-                    gameMap[i][j] = PELLET;
-                } else {
-                    gameMap[i][j] = EMPTY;
+                int randNum = rand() % 5; // Generate random number from 0 to 9
+                if (randNum == 2) 
+                {
+                    gameMap[i][j] = 2 + rand() % 3; // Randomly assign power-up value (2, 3, or 4)
+                } 
+                else
+                {
+                    gameMap[i][j] = 0;
                 }
             }
         }
@@ -116,25 +125,25 @@ void* userInput(void* arg) {
             pthread_mutex_unlock(&inputMutex);
             pthread_exit(NULL);
         }
-        if (event->type == sf::Event::KeyPressed || event->type == sf::Event::KeyReleased) 
+        if (event->type == sf::Event::KeyPressed) 
         {
             switch(event->key.code)
             {
                 case sf::Keyboard::Up:
-                    pacman_shape->move(0, -10);
+                    pacman_shape->move(0, CELL_SIZE * -1);
                     break;
                 case sf::Keyboard::Down:
-                    pacman_shape->move(0, 10);
+                    pacman_shape->move(0, CELL_SIZE);
                     break;
                 case sf::Keyboard::Left:
-                    pacman_shape->move(-10, 0);
+                    pacman_shape->move(CELL_SIZE*-1, 0);
                     break;
                 case sf::Keyboard::Right:
-                    pacman_shape->move(10, 0);
+                    pacman_shape->move(CELL_SIZE, 0);
                     break;
             }
-            event->key.code = sf::Keyboard::Unknown;
         }
+        event->key.code = sf::Keyboard::Unknown;
         pthread_mutex_unlock(&inputMutex);
         sleep(sf::milliseconds(100));
     }
@@ -148,10 +157,19 @@ int main() {
     srand(time(nullptr));
     // Initialize game board
     initializeGameBoard();
+    //cout grif
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            cout << gameMap[i][j] << " ";
+        }
+        cout << endl;
+    }
     // Create SFML window
-    sf::CircleShape pacman_shape(30);
+    sf::CircleShape pacman_shape(25/2);
     pacman_shape.setFillColor(sf::Color::Yellow);
-    pacman_shape.setPosition(100, 100);
+    pacman_shape.setPosition(CELL_SIZE + 25/8, CELL_SIZE + 25/4);
 
     // Create thread for user input
     pthread_t userInputThread;
@@ -162,7 +180,7 @@ int main() {
     pthread_create(&userInputThread, nullptr, userInput, args);
 
     // Create SFML window
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML window");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML window");
 
     // Initialize game grid
 
@@ -177,8 +195,8 @@ int main() {
         pthread_mutex_unlock(&inputMutex);
 
         // Clear, draw, and display
-        drawGrid(window);
         window.clear();
+        drawGrid(window);
         window.draw(pacman_shape);
         window.display();
     }
